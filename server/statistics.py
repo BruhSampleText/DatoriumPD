73f43d296e258199fb91146d61a0f8db140ab192
Neon <bob12331rits@gmail.com>
2025-03-29 01:30:43 +0200

    More statistics

--- server/statistics.py
+++ server/statistics.py
@@ -1,10 +1,14 @@
 from server import admin, database, application;
 
-import matplotlib.pyplot as plot;
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
 import pandas as panda;
 import flask;
+import io;
 
-def generate_histogram():
+@application.route( "/histogram" )
+def serve_histogram():
 	data_frame = panda.DataFrame( 
 		list(
 			database.PostDB.select()
@@ -12,16 +16,23 @@ def generate_histogram():
 		)
 	)
 
-	print( data_frame )
 	data_frame[ "date" ] = panda.to_datetime( data_frame[ "date" ] )
 	data_frame[ "weekday" ] = data_frame[ "date" ].dt.day_name(  )
 
-	plot.hist( data_frame[ "weekday" ], bins = 7 )
-	plot.xlabel( "Days of the week" )
-	plot.ylabel( "Entries" )
-	plot.title( "Lost entry distributin per day" )
-	plot.savefig( "server/static/images/dynamic/hist.svg" )
-	plot.close()
+	figure = Figure()
+	axis = figure.subplots()
+
+	axis.hist( data_frame[ "weekday" ], bins = 7 )
+	axis.set_title( "Lost entry distribution per day" )
+	axis.set_xlabel( "Days of the week" )
+	axis.set_ylabel( "Entries" )
+
+	img = io.BytesIO()
+	figure.savefig( img, format="png" )
+	img.seek( 0 )
+
+	return flask.Response(img.getvalue(), mimetype="image/png")
+
 
 def generate_most_used_tag_per_day_heatmap():
 	data_frame = panda.DataFrame(
@@ -43,7 +54,4 @@ def generate_most_used_tag_per_day_heatmap():
 
 @application.route( "/statistics" )
 def	route_statistics():
-	generate_histogram()
-	#generate_most_used_tag_per_day_heatmap()
-	#generate_histogram_entries()
 	return flask.render_template( "statistics.html" )
